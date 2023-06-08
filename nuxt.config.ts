0bf77060a5ecdd240fff7d7d9782dfecd2c7046a
Colin Rabyniuk <colinxr@gmail.com>
2023-06-08 14:57:47 -0700

    edit content titles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    // pageTransition: { name: "slide-right", mode: 'in-out'},
-  },
   css: ['@/assets/css/tailwind.css', '@/assets/css/grid.css', '@/assets/css/content.scss'],
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss'
   ],
+  nitro: {
+    prerender: {
+      crawlLinks: true
+    }
+  }
 })
