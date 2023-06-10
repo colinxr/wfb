9bff10e1f2fad328d9e040f8cd241783f0cc4530
Colin Rabyniuk <colinxr@gmail.com>
2023-06-10 16:41:09 -0700

    change github yml action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
     },
-    baseURL: "/wfb"
   },
   css: ['@/assets/css/tailwind.css', '@/assets/css/grid.css', '@/assets/css/content.scss'],
   modules: [
