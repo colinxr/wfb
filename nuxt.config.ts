9f9fa70e9539801fccfdff69b6de7d60a3befc65
Colin Rabyniuk <colinxr@gmail.com>
2023-06-10 17:58:06 -0700

    nuxt config trial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
     },
+    
   },
   css: ['@/assets/css/tailwind.css', '@/assets/css/grid.css', '@/assets/css/content.scss'],
   modules: [
